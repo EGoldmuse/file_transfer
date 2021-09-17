a67e7a0a0b709e0abae4aecd55f61c5f8e40fc53
EGoldmuse <eli@nishmamusic.com>
2021-09-17 13:25:02 -0500

    first commit

---
+++ receiver_folder/client.py
@@ -0,0 +1,21 @@
+import socket
+
+s= socket.socket()
+
+host = input(str("Please enter the host address of the sender: "))
+
+port = 8080
+s.connect((host, port))
+print("Connected...")
+
+filename = input(str("Please enter the name of the incoming file: "))
+
+file = open(filename, 'wb')
+
+file_data = s.receive(1024)
+
+file.write(file_data)
+
+file.close()
+
+print("File has been received successfully!")
